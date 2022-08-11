436aab41035155c4e0776e042694e56f1a462d0b
rodrigoaguilar11 <110432593+rodrigoaguilar11@users.noreply.github.com>
2022-08-11 08:29:35 -0300

    Add files via upload

---
+++ js/validacion.js
@@ -0,0 +1,7 @@
+function showAlertSuccess() {
+    document.getElementById("alert-success").classList.add("show");
+}
+
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
