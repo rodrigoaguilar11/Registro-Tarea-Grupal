06b898854bd4b31190d21d95aa5cb18eab47be43
Rodrigo <leonelaguilardsalinas@gmail.com>
2022-08-11 11:57:23 -0300

    actualizacion

--- js/validacion.js
+++ js/validacion.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     //Boton para registrarse
     document.getElementById("regBtn").addEventListener("click", () => {
         register();
+
+
+
     })
 });
 
@@ -10,29 +13,49 @@ function register() {
     let errors = document.querySelector('#errors');
     let passerror = document.querySelector('#passerror');
     let completo = document.querySelector('#completo');
+    let contraseñaigual = document.querySelector('#contraseñaigual');
 
     let password1 = document.getElementById("password1").value;
     let password2 = document.getElementById("password2").value;
     let password;
+
+
     if (password1 != password2) {
         errors.textContent = "Las contraseñas deben ser iguales";
-
     } else if (password1.length < 6) {
         errors.textContent = "La contraseña debe ser mayor o igual a 6";
     } else {
         password = password1;
+        errors.textContent = "";
+
     }
     let nombre = document.getElementById("nombre").value;
     let apellido = document.getElementById("apellido").value;
     let email = document.getElementById("email").value;
 
+    //Alertas
+    if (nombre === "" || apellido === "" || email === "" || (!email.includes("@") || !email.includes(".com")) || password === undefined || !document.getElementById('terminos').checked) {
+        showAlertError();
+    } else {
+        showAlertSuccess();
+    }
+
+    if (password1 === password2 && password1 != "") {
+        contraseñaigual.textContent = "Las contraseñas son iguales";
+    }
+
+    //comparacion
     if (nombre === "") {
         errors.textContent = 'Ingrese Nombre';
+    } else if (parseInt(nombre)) {
+        errors.textContent = 'No se permiten numeros en el nombre';
     } else if (apellido === "") {
         errors.textContent = 'Ingrese Apellido';
+    } else if (parseInt(apellido)) {
+        errors.textContent = 'No se permiten numeros en el apellido';
     } else if (email === "") {
         errors.textContent = 'Ingrese Email';
-    } else if (!email.includes("@")||!email.includes(".com")) {
+    } else if (!email.includes("@") || !email.includes(".com")) {
         errors.textContent = 'Formato de mail incorrecto';
     } else if (password === undefined) {
         passerror.textContent = 'Ingrese Contraseña';
