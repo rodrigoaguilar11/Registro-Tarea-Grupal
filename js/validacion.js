f6f4e853908c5ac3130a6b794ed0af69eca85b2b
Rodrigo <leonelaguilardsalinas@gmail.com>
2022-08-11 08:59:17 -0300

    aceptar terminos

--- js/validacion.js
+++ js/validacion.js
@@ -12,15 +12,14 @@ function register() {
     let nombre = document.getElementById("nombre").value;
     let apellido = document.getElementById("apellido").value;
     let email = document.getElementById("email").value;
-let terminos = document.getElementById("terminos").value;
     if (nombre === "") {
         alert("Ingrese Nombre");
     } else if (apellido === "") {
         alert("Ingrese Apellido");
     } else if (email ===""){
         alert("Ingrese Email");
-    } else if (terminos ==true){
-        alert("Ingrese Email");
+    } else if (!document.getElementById('terminos').checked){
+        alert("Acepte los terminos");
 
     }else{
 alert("Se registro correctamente");
