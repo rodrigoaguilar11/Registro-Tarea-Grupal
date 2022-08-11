593ada97c384c9bfa77ff7642dd3ffa051ef62ef
Rodrigo <leonelaguilardsalinas@gmail.com>
2022-08-11 08:54:32 -0300

    cambios de rodrigo

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+    //Inicio del DOMContentLoaded
+    //Boton para registrarse
+    document.getElementById("regBtn").addEventListener("click", () => {
+        register();
+    })
+
+    //Fin del DOMContenLoaded
+});
+
+function register() {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let email = document.getElementById("email").value;
+let terminos = document.getElementById("terminos").value;
+    if (nombre === "") {
+        alert("Ingrese Nombre");
+    } else if (apellido === "") {
+        alert("Ingrese Apellido");
+    } else if (email ===""){
+        alert("Ingrese Email");
+    } else if (terminos ==true){
+        alert("Ingrese Email");
+
+    }else{
+alert("Se registro correctamente");
+    }
+}
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
