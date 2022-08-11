89dd7a909c8ef81b7118d2ff4d276ffac05e3c1f
Rodrigo <leonelaguilardsalinas@gmail.com>
2022-08-11 10:42:40 -0300

    cambios

--- js/validacion.js
+++ js/validacion.js
@@ -1,39 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
-    //Inicio del DOMContentLoaded
     //Boton para registrarse
     document.getElementById("regBtn").addEventListener("click", () => {
         register();
     })
-
-    //Fin del DOMContenLoaded
 });
 
 function register() {
+    //parrafo de los errores
+    let errors = document.querySelector('#errors');
+    let passerror = document.querySelector('#passerror');
+    let completo = document.querySelector('#completo');
+
     let password1 = document.getElementById("password1").value;
     let password2 = document.getElementById("password2").value;
     let password;
     if (password1 != password2) {
-        alert("La contraseñas deben ser iguales");
+        errors.textContent = "Las contraseñas deben ser iguales";
+
     } else if (password1.length <6) {
-        alert("La contraseña debe ser mayor o igual a 6");
+        errors.textContent = "La contraseña debe ser mayor o igual a 6";
+
     } else {
         password = password1;
     }
     let nombre = document.getElementById("nombre").value;
     let apellido = document.getElementById("apellido").value;
     let email = document.getElementById("email").value;
+
     if (nombre === "") {
-        alert("Ingrese Nombre");
+        errors.textContent = 'Ingrese Nombre';
     } else if (apellido === "") {
-        alert("Ingrese Apellido");
+        errors.textContent = 'Ingrese Apellido';
     } else if (email === "") {
-        alert("Ingrese Email");
+        errors.textContent = 'Ingrese Email';
     } else if (password === undefined) {
-        alert("Ingrese Contraseña");
+        passerror.textContent = 'Ingrese Contraseña';
     } else if (!document.getElementById('terminos').checked) {
-        alert("Debe aceptar los terminos");
+        passerror.textContent = '';
+        errors.textContent = 'Debe aceptar los terminos';
     } else {
-        alert("Se registro correctamente");
+        completo.textContent = "Se registro correctamente";
+        errors.textContent = '';
     }
 }
 
